248ffd188e9f43b73b49127a307027cfefbbebe4
austernotus <76696499+austernotus@users.noreply.github.com>
2024-06-07 18:33:10 +0100

    added constructor info for Book with info return

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 const myLibrary = [];
 
-function Book(){
-
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () =>{
+        this.isRead = this.read ? "read." : "not read yet.";
+        return `${this.title}, by ${this.author}, ${this.pages} pages, ${this.isRead}`
+    }
 }
 
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", false)
+
 function addBookToLibrary(){
-    
+
 }
